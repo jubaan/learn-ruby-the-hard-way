826a3d53bacdc8710607a890d7e9246864858c6b
jubaan <julio.ab@outlook.com>
2020-02-04 15:01:15 -0600

    Add: ex01 file with drills

---
+++ ex01_a_good_first_program.rb
@@ -0,0 +1,27 @@
+# A Good First Program
+puts 'Hello World!'
+puts 'Hello Again'
+puts 'I like typing this.'
+puts 'This is fun.'
+puts 'Yay! Printing'
+puts "I'd much rather you 'not'."
+puts 'I "said" do not touch this.'
+
+# Study Drills
+
+# Make your script print another line.
+
+puts 'This is the other line.'
+
+# Make your script print only one of the lines.
+
+# puts 'Hello World!'
+# puts 'Hello Again'
+# puts 'I like typing this.'
+puts 'This is fun.'
+# puts 'Yay! Printing'
+# puts "I'd much rather you 'not'."
+# puts 'I "said" do not touch this.'
+
+# Put a # (octothorpe) character at the beginning of a line. What did it do? Try to find out what this character does.
+# The # (octothorpe) is mainly for oneline comments
