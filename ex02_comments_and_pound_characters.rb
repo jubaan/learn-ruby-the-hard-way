bdbb50fa9bc6050810b48c63c6cdc428bcf14058
jubaan <julio.ab@outlook.com>
2020-02-04 16:53:07 -0600

    Add: ex04 file
    Lear about how to declare variables and how to use them in a starting

--- ex02_comments_and_pound_characters.rb
+++ ex02_comments_and_pound_characters.rb
@@ -15,7 +15,7 @@ puts 'This will run.'
 # 1) Find out if you were right about what the # character does and make sure
 #    you know what it's called (octothorpe or pound character).
 
-## Octothorpe, Hashtag, Hash, Sharp, Square, Hex. source http://www.todayifoundout.com/index.php/2014/11/origin-hashtag-symbol/
+# Octothorpe, Hashtag, Hash, Sharp, Square, Hex. source http://www.todayifoundout.com/index.php/2014/11/origin-hashtag-symbol/
 
 # 2) Take your ex2.rb file and review each line going backward. Start at the
 #    last line and check each word in reverse against what you should have
@@ -23,7 +23,7 @@ puts 'This will run.'
 
 # 3) Did you find more mistakes? Fix them.
 
-## No
+# No
 
 # 4) Read what you typed above out loud, including saying each character by its
 #    name. Did you find more mistakes? Fix them.
