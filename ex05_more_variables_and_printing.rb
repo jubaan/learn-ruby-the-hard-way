6e55bc1ab95348e70e895c53269b7fd48313ef7f
jubaan <julio.ab@outlook.com>
2020-02-04 19:19:05 -0600

    Add: more variables and printing
    learn how to use variables and insert them in strings, also how to make
    operations using the #{...}

---
+++ ex05_more_variables_and_printing.rb
@@ -0,0 +1,51 @@
+# More variables and printing
+
+my_name = 'Zed A. S_haw'
+my_age = 35 #  not a lie in 2009
+my_height = 74 # inches
+my_weight = 180 # lbs
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+puts "Let's talk about #{my_name}."
+puts "He's #{my_height} inches tall."
+puts "He's #{my_weight} pounds heavy."
+puts "Actaully that's not tha heavy."
+puts "He's got #{my_eyes} eyes and #{my_hair} hair."
+puts "His teeth are usually #{my_teeth} depending on the coffe."
+
+# this line is tricky, try to get it exactly right
+puts "If I add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height + my_weight}."
+
+# Study Drills
+
+# 1) Change all the variables so there is no my_ in front of each one. Make sure
+# you change the name everywhere, not just where you used = to set them.
+
+name = 'Zed A. S_haw'
+age = 35 #  not a lie in 2009
+height = 74 # inches
+weight = 180 # lbs
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+
+puts "Let's talk about #{name}."
+puts "He's #{height} inches tall."
+puts "He's #{weight} pounds heavy."
+puts "Actaully that's not tha heavy."
+puts "He's got #{eyes} eyes and #{hair} hair."
+puts "His teeth are usually #{teeth} depending on the coffe."
+
+# this line is tricky, try to get it exactly right
+puts "If I add #{age}, #{height}, and #{weight} I get #{age + height + weight}."
+
+# 2) Try to write some variables that convert the inches and pounds to centimeters
+# and kilograms. Do not just type in the measurements. Work out the math in
+# Ruby.
+
+lbs_to_kg = my_weight * 0.45
+in_to_cm = my_height * 2.54
+
+puts lbs_to_kg, in_to_cm
