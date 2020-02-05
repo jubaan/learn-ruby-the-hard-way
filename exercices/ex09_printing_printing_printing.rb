16317aeceb6a166d088a85a9fc5b9495b1801638
jubaan <julio.ab@outlook.com>
2020-02-04 23:06:20 -0600

    Add: ex09 printing printing printing
    Learn about the use escape characters and %q{...} for printing oneline or
    multiline text that also could have single or double quotes

---
+++ exercices/ex09_printing_printing_printing.rb
@@ -0,0 +1,35 @@
+# Printing, printing, printing
+
+# Here's some new strange strugg, remember type it exactly.
+
+days = 'Mon Tue Wed Thu Fri Sat Sun'
+
+# the "\n" is an escape character that creates a newline
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+puts "Heree are the days: #{days}"
+puts "Here are the months:#{months}"
+
+# the %q{...} is used for printing oneline or multiline text with both single and double quotes
+puts %q{
+  There's sometihng going on here.
+  With this wird quote
+  We'll be able to type as much as we like.
+  Even 4 lines if we want, or 5, or 6.
+}
+
+# Study Drills
+
+# 1) Go back through and write a comment on what each line does.
+
+# 2) Read each one backward or out loud to find your errors.
+
+# 3) From now on, when you make mistakes, write down on a piece of paper what
+#    kind of mistake you made.
+
+# 4) When you go to the next exercise, look at the mistakes you have made and
+#    try not to make them in this new one.
+
+# 5) Remember that everyone makes mistakes. Programmers are like magicians who
+#    fool everyone into thinking they are perfect and never wrong, but it's all
+#    an act. They make mistakes all the time.
